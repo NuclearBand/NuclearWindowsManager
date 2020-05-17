7e27aaac69763985f99ecd71cd5b7fce31b02e3f
Tr0sT <tr0st@bk.ru>
2020-05-17 19:03:01 +0300

    запихиваем в assets для редактирования

---
+++ Assets/com.nuclearband.windowsmanager/Samples/Sample/Example1/Example1.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace NuclearBand
+{
+    public class Example1 : MonoBehaviour
+    {
+        void Start()
+        {
+            var rootPath = "NuclearBand/Examples_WindowsManager/Example1/";
+            WindowsManager.Init(new WindowsManagerSettings()
+            {
+                RootPath = rootPath + "Canvas",
+                InputBlockPath = rootPath + "InputBlocker"
+            });
+            WindowsManager.CreateWindow(Example1Window1.Path);
+        }
+    }
+}
---
+++ Assets/com.nuclearband.windowsmanager/Samples/Sample/Example2/Example2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace NuclearBand
+{
+    public class Example2 : MonoBehaviour
+    {
+        void Start()
+        {
+            var rootPath = "NuclearBand/Examples_WindowsManager/Example1/";
+            WindowsManager.Init(new WindowsManagerSettings()
+            {
+                RootPath = rootPath + "Canvas",
+                InputBlockPath = rootPath + "InputBlocker"
+            });
+            var window = WindowsManager.CreateWindow(Example2Window1.Path).Window;
+        }
+    }
+}
