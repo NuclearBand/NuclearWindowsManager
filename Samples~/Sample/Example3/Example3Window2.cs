a44027f58a7e4186f8abeaa6694635e9fa7470ba
Tr0sT <tr0st@bk.ru>
2020-05-17 19:03:01 +0300

    запихиваем в assets для редактирования

---
+++ Samples~/Sample/Example3/Example3Window2.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+
+namespace NuclearBand
+{
+    public class Example3Window2 : Example2Window2
+    {
+        public new const string Path = "NuclearBand/Examples_WindowsManager/Example3/TestWindow2";
+        public Text rebuildText;
+        int rebuildNum;
+
+        class Example3Window2_WindowTransientData : WindowTransientData
+        {
+            readonly int rebuildNum;
+
+            public Example3Window2_WindowTransientData(Window window) : base(window)
+            {
+                rebuildNum = (window as Example3Window2).rebuildNum;
+            }
+
+            public override void RestoreWindow(Window window)
+            {
+                base.RestoreWindow(window);
+                (window as Example3Window2).rebuildNum = rebuildNum;
+            }
+        }
+
+        public override WindowTransientData GetTransientData()
+        {
+            return new Example3Window2_WindowTransientData(this);
+        }
+
+        public override void Init()
+        {
+            base.Init();
+            rebuildNum = 0;
+            DrawRebuildNum();
+        }
+
+        public override void InitAfterRebuild(WindowTransientData windowTransientData)
+        {
+            base.InitAfterRebuild(windowTransientData);
+            text.text = title;
+            ++rebuildNum;
+            DrawRebuildNum();
+        }
+
+        void DrawRebuildNum()
+        {
+            rebuildText.text = rebuildNum.ToString();
+        }
+    }
+}
